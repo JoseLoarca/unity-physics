62c87ccee33afbde38cec6964ba0074f993b1e16
José Loarca <joseloarca97@icloud.com>
2017-07-16 17:26:39 -0600

    finished basic tutorial

---
+++ Assets/Scripts/CoinStuff.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+public var points : int = 50;
+
+function OnTriggerEnter2D(other	: Collider2D)
+{
+	Debug.Log("Triggered! Worth " + points + " points.");
+
+	Destroy(gameObject);
+}
